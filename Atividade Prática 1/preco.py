ab125de5a837d1f67116ad776ed40c89d5c45bf6
Luis <felipeserradas@gmail.com>
2026-01-09 19:20:39 -0300

    Atividade Prática 01

---
+++ Atividade Prática 1/preco.py
@@ -0,0 +1,20 @@
+"""
+4- Calculadora de Preço Total
+* Desenvolva um programa que calcula o preço total de uma compra. Use as seguintes informações:
+
+* Nome do produto: "Cadeira Infantil"
+* Preço unitário: R$ 12.40
+* Quantidade: 3
+O programa deve calcular o preço total e exibir todas as informações, incluindo o resultado final.
+"""
+
+nomeProduto = "Cadeira Infantil"
+precoUnitario = 12.40
+quantidade = 3
+
+print(f"""
+    Produto: {nomeProduto}
+    Preço únitário: {precoUnitario}
+    Quantidade: {quantidade}
+    Total a pagar: {precoUnitario * quantidade}
+""")
